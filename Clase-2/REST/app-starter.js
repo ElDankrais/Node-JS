f410f9e8c52ecab6d6d4903283d938ad2cf57d81
Daniel Chanci <daniel.chanci@kinesso.com>
2023-10-14 11:53:19 -0500

    Adding corrections

--- Clase-2/REST/app-starter.js
+++ Clase-2/REST/app-starter.js
@@ -7,3 +7,106 @@ const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin: (origin, callback) => {
+        console.log(origin);
+        const ACCEPTED_ORIGINS = [
+            'http://localhost:8080',
+            'http://localhost:1234',
+            'http://localhost:5500',
+            'http://127.0.0.1:5500'
+        ]
+
+        if (ACCEPTED_ORIGINS.includes(origin)) {
+            console.log('yey');
+            return callback(null, true);
+        }
+
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        return callback(new Error('Invalid origin'));
+    }
+}))
+
+//Devolver las películas con /movies
+app.get('/movies', (req, res) => {
+    res.json(movies);
+})
+
+//Devolver películas en específico /movies/:id
+app.get('/movies/:id', (req, res) => {
+    const { id } = req.params;
+    const movie = movies.find(movie => movie.id === id);
+    if (movie) return res.json(movie);
+    return res.status(404).json({message: 'Pailander'});
+});
+
+app.post('/movies/', (req, res) => {
+    const result = validateMovie(req.body);
+    
+    if(!result.success) {
+        return res.status(400).json({error: JSON.parse(result.error.message)});
+    }
+
+    // en base de datos
+    const newMovie = {
+        id: crypto.randomUUID(),
+        ...result.data
+    }
+
+    movies.push(newMovie);
+
+    res.status(201).json(newMovie);
+})
+
+//Editar una propiedad en específico
+
+app.patch('/movies/:id', (req, res) => {
+    const result = validatePartialMovie(req.body);
+
+    if(!result.success) {
+        return res.status(400).json({error: JSON.parse(result.error.message)});
+    }
+
+    const { id } = req.params;
+
+    const movieIndex = movies.findIndex(movie => movie.id === id);
+    
+    //NO LO ENCONTRÓ
+    if(movieIndex === -1) {
+        return res.status(404).json({message: 'Tu película no está'});
+    }
+
+    const updatedMovie = {
+        ...movies[movieIndex],
+        ...result.data
+    }
+
+    movies[movieIndex] = updatedMovie;
+
+    return res.json(updatedMovie);
+});
+
+
+app.delete('/movies/:id', (req, res) => {
+    const { id } = req.params;
+
+    const movieIndex = movies.findIndex(movie => movie.id === id);
+    
+    //NO LO ENCONTRÓ
+    if(movieIndex === -1) {
+        return res.status(404).json({message: 'Tu película no está'});
+    }
+
+    movies.splice(movieIndex, 1);
+
+    return res.json({message: 'Se murióooooo'});
+})
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
--- Clase-2/express/app-starter.js
+++ Clase-2/express/app-starter.js
@@ -4,3 +4,41 @@ const path = require('node:path');
 
 const adminRoutes = require('./routes/admin');
 const messageRoutes = require('./routes/message');
+
+const PORT = process.env.PORT || 1234;
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
+
+app.use('/admin', adminRoutes);
+app.use(messageRoutes);
+
+//path variable
+app.get('/r/:subreddit', (req, res) => {
+    console.log(req.params);
+    const subreddit = req.params.subreddit;
+    res.send(`<h1>${subreddit}</h1>`);
+})
+
+//query params
+
+app.get('/search', (req, res) => {
+    const {id} = req.query;
+    res.send(`<h1>${id}</h1>`);
+});
+
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname,  'web', 'index.html'));
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname,  'web', '404.html'));
+});
+
+
+
+app.listen(PORT, () => {
+    console.log('listening on port: ' + PORT);
+
+});
